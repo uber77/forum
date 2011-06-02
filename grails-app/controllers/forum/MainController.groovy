df0e00aff5ac0f1c0e663849692a436d12e1a29d
EmilianoSDD <eviera@sdd.com.ar>
2011-06-02 11:03:37 -0300

    uso de tag libraries en el codigo

--- grails-app/controllers/forum/MainController.groovy
+++ grails-app/controllers/forum/MainController.groovy
@@ -4,7 +4,10 @@ class MainController {
 	//Si se pone un atributo de esta clase, y no se devuelve nada en el def index, se puede acceder al atributo
 
     def index = { 
-    	[date : new Date()] //se le devuelve a la vista un map para que lo pueda usar
+		//los taglibs se pueden llamar desde aca!
+		def fechaFormateada = g.formatDate(format:"yyyy MM dd", date:new Date())
+
+    	[date : fechaFormateada] //se le devuelve a la vista un map para que lo pueda usar
     
     }
 }
